439a278ad09fa69ef3828cf822b44ef8edac428d
jeffmazz <jeff.mazzotta@hotmail.com>
2024-11-20 22:34:22 -0300

    Feat: Começando a home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,32 @@
 import React from "react"
-
+import styles from "./Home.module.css"
 const Home = () => {
 
     return (
+
         <>
-            WELCOME
+
+            <div className={styles.container}>
+
+                <section className={styles.infos}>
+                    <h3 className={styles.title}> Wall-e </h3>
+                    <p className={styles.sinopse}> Após entulhar a Terra de lixo e poluir a atmosfera com gases tóxicos, a humanidade deixou o planeta e passou a viver em uma gigantesca nave. O plano era que o retiro durasse alguns poucos anos, com robôs sendo deixados para limpar o planeta. WALL-E é o último destes robôs, e sua vida consiste em compactar o lixo existente no planeta. Até que um dia surge repentinamente uma nave, que traz um novo e moderno robô: Eva. A princípio curioso, WALL-E se apaixona e resolve segui-la por toda a galáxia. </p>
+                </section>
+
+                <section className={styles.carousel}>
+                    <div className={styles.carousel_item}>
+                        
+                    </div>
+                    <div className={styles.carousel_item}>
+
+                    </div>
+                    <div className={styles.carousel_item}>
+
+                    </div>
+                </section>
+
+            </div>
+            
         </>
     )
 }
