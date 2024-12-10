6bde7166ba4c013d5b6bf06cf058bb799c93bab6
Jefferson Mazzotta <jeff.mazzotta@hotmail.com>
2024-12-10 14:07:01 -0300

    Update main.jsx
    Verificando Environments Variables

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Console API KEY
+console.log('API KEY:', import.meta.env.VITE_TMDB_API_KEY)
+
 createRoot(document.getElementById('root')).render(
   //<StrictMode>
     <App />
