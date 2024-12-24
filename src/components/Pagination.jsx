61f0fbd2b7ac2269f2b7fd576a52d400a9c3355c
jeffmazz <jeff.mazzotta@hotmail.com>
2024-12-23 22:24:43 -0300

    Enhancement: aprimorando o componente Pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "./Pagination.module.css"
 
 import { GrCaretNext } from "react-icons/gr";
@@ -10,30 +10,55 @@ import { GrChapterPrevious } from "react-icons/gr";
 
 const Pagination = ({ totalPages, actualPage, setActualPage }) => {
 
-    const goToPage = (page) => {
-        setActualPage(page);
+    const [inputValue, setInputValue] = useState(actualPage)
+
+    const handlePageChange = (page) => {
+        if(page >= 1 && page <= totalPages) {
+            setActualPage(page)
+            setInputValue(page)
+        }
+    }
+
+    const handleInputChange = (e) => {
+        const value = e.target.value
+        if(/^\d*/.test(value)) {
+            setInputValue(value === "" ? "" : Number(value))
+        }
+    }
+
+    const handleInputSubmit = (e) => {
+        e.preventDefault()
+        if(inputValue >= 1 && inputValue <= totalPages) {
+            setActualPage(inputValue)
+        } else {
+            setInputValue(actualPage)
+        }
     }
 
     return (
         <div className={styles.pagination_container}>
 
-            <button onClick={() => goToPage(1)} disabled={actualPage === 1} aria-label="Go to page 1">
+            <button onClick={() => handlePageChange(1)} disabled={actualPage === 1} aria-label="Go to page 1">
                 <GrChapterPrevious/>
             </button>
 
-            <button onClick={() => goToPage(Number(actualPage) - 1)} disabled={actualPage === 1} aria-label="Go to previous page">
+            <button onClick={() => handlePageChange(Number(actualPage) - 1)} disabled={actualPage === 1} aria-label="Go to previous page">
                 <GrCaretPrevious/>
             </button>
 
             <div className={styles.actual_page}>
-                {actualPage}
+                <form onSubmit={handleInputSubmit}>
+                    <input type="text" value={inputValue} onChange={handleInputChange}/>
+                </form>
+                <span> of </span>
+                <span> {totalPages} </span>
             </div>
             
-            <button onClick={() => goToPage(Number(actualPage) + 1)} disabled={actualPage === totalPages} aria-label="Go to next page">
+            <button onClick={() => handlePageChange(Number(actualPage) + 1)} disabled={actualPage === totalPages} aria-label="Go to next page">
                 <GrCaretNext/>
             </button>
 
-            <button onClick={() => goToPage(totalPages)} disabled={actualPage === totalPages} aria-label="Go to last page">
+            <button onClick={() => handlePageChange(totalPages)} disabled={actualPage === totalPages} aria-label="Go to last page">
                 <GrChapterNext/>
             </button>
 
